0550a9d5489a1d7b4877d7dfc318200624cc8595
Perkó Zsolt <zsperko@gmail.com>
2017-10-24 22:35:48 +0200

    animated page load

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ function fadeDone(){
 //Eseménykezelő beállítása
 $("p").click(function(){
     $(this).hide();
-    $(this).fadeTo(4000,1, fadeDone);
+    //$(this).fadeTo(4000,1, fadeDone);
+    $(this).slideDown(3500);
 });
 
 //Esemény beállítása
@@ -15,5 +16,10 @@ $("p").click();
 //Kattintás megelőzés
 $("nav a.nav-link").click( function(ev){
     ev.preventDefault();
-    console.log(ev);
+    var link = $(this);
+    $(document.body).animate({
+        opacity: '0'
+    },3000, function(){
+        document.location = link.attr("href");
+    });
 });
