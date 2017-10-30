695abbdad16106c5820c8b679e8adf7152ccbc05
Perkó Zsolt <zsperko@gmail.com>
2017-10-30 15:36:43 +0100

    jQuery chaining, .on esemény stb

--- js/main.js
+++ js/main.js
@@ -5,21 +5,58 @@ function fadeDone(){
 
 //Eseménykezelő beállítása
 $("p").click(function(){
-    $(this).hide();
+    //$(this).hide();
     //$(this).fadeTo(4000,1, fadeDone);
-    $(this).slideDown(3500);
+    //$(this).slideDown(3500).css("color","blue");
 });
 
 //Esemény beállítása
-$("p").click();
+//$("p").click();
 
 //Kattintás megelőzés
 $("nav a.nav-link").click( function(ev){
     ev.preventDefault();
     var link = $(this);
-    $(document.body).animate({
-        opacity: '0'
-    },3000, function(){
+    var prop    = link.data("prop");
+    var val     = link.data("value");
+    var speed   = link.data("speed");
+    var settings = {};
+    settings[prop]=val;
+
+    $(document.body).animate(settings, speed, function(){
         document.location = link.attr("href");
     });
 });
+
+//Event oldal
+$(".events-search-row input").on("keyup", function(ev){
+    var elem;
+    var search;
+    var content;
+    $.each($(".events-card-deck .card .card-title"),function(index,elem){
+      elem = $(elem);
+      search = ev.target.value.toLowerCase();
+      content = elem.html().toLocaleLowerCase();
+      if (content.indexOf(search) == -1){
+        elem.parents(".card").hide();
+      } else {
+        elem.parents(".card").show();
+      }
+    });
+  }
+);
+
+//Regiszter oldal
+$(".cherry-custom-file").on(
+    "change", function(ev){
+      var name = ev.target.value.split("\\").pop();
+      $(this)
+        .find(".file-name")
+        .html(name);
+    }
+  );
+
+//Ticket oldal
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
